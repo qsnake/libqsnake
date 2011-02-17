9c81787f597c394406a6221c06b23213590ae012
Ondrej Certik <ondrej@certik.cz>
2011-02-16 18:38:18 -0800

    Orig mesh

--- examples/pb.py
+++ examples/pb.py
@@ -2,10 +2,10 @@ from qsnake.atom import solve_hydrogen_like_atom
 
 Z = 82
 mesh_params = {
-        "r_min": 5e-8,
-        "r_max": 20,
-        "a": 20000,
-        "N": 30000,
+        "r_min": 1e-10,
+        "r_max": 42.4813927645,
+        "a": 7928200510.27,
+        "N": 5382,
         }
 solver_params = {
         "solver": "elk",
