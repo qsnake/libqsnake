546cec2c6cfdf5149ee4fe507bf43d878c6b1d6a
Ondrej Certik <ondrej@certik.cz>
2011-02-18 12:14:16 -0800

    Full optimization

--- examples/pb.py
+++ examples/pb.py
@@ -1,6 +1,7 @@
 from qsnake.atom import solve_hydrogen_like_atom, ConvergeError
 from qsnake.mesh import n_minimize
 from sympy import TableForm
+from numpy import arange
 
 def optimize(r_min=1e-6, r_max=50, N=700, solver="elk"):
     def f(a):
@@ -24,8 +25,8 @@ def optimize(r_min=1e-6, r_max=50, N=700, solver="elk"):
     a_min = 1
     a_max = 1e8
     # temporary:
-    a_min = 3982631.666582 - 1
-    a_max = 3982631.666582 + 1
+    #a_min = 3982631.666582 - 1
+    #a_max = 3982631.666582 + 1
     params = [r_min, r_max, N]
     done = False
     while not done:
@@ -40,16 +41,21 @@ def optimize(r_min=1e-6, r_max=50, N=700, solver="elk"):
     print
     return a_opt, error
 
-def optimize_mesh():
-    r_min = 1e-8
+def optimize_mesh(r_min=1e-8):
     r_max = 50
     N = 700
-    solver = "elk"
+    solver = "dftatom"
 
-    a, error = optimize(r_min=1e-6, r_max=50, N=700, solver="elk")
+    a, error = optimize(r_min=1e-6, r_max=50, N=700, solver=solver)
     print "'a' is optimized:"
     print TableForm([[r_min], [r_max], [a], [N]],
                 headings=(("r_min", "r_max", "a", "N"), ("Mesh parameters",)))
     print "total error:", error
 
-optimize_mesh()
+a = 1e-10
+b = 1e-6
+r_min_list = arange(a, b, (b-a)/20)
+for r_min in r_min_list:
+    print "Calculating:", r_min
+    optimize_mesh(r_min)
+    print "Done calculating:", r_min
