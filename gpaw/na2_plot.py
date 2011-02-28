36572b21b4e5c94b4c9b26c02daf046e3b8e765c
Ondrej Certik <ondrej@certik.cz>
2011-02-28 00:40:25 -0800

    Show a nice graph

--- gpaw/na2_plot.py
+++ gpaw/na2_plot.py
@@ -8,8 +8,8 @@ osz_y = a[:, 3]
 osz_z = a[:, 4]
 
 plot(om, osz, label="osz")
-plot(om, osz_x, label="osz_x")
-plot(om, osz_y, label="osz_y")
-plot(om, osz_z, label="osz_z")
+#plot(om, osz_x, label="osz_x")
+#plot(om, osz_y, label="osz_y")
+#plot(om, osz_z, label="osz_z")
 legend()
 show()
