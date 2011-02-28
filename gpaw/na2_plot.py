1ab218675842acd2948a7a713c485cffa57dbc37
Ondrej Certik <ondrej@certik.cz>
2011-02-28 00:37:11 -0800

    Plot Na2 spectra

---
+++ gpaw/na2_plot.py
@@ -0,0 +1,15 @@
+from numpy import loadtxt
+from pylab import plot, show, legend
+a = loadtxt("Na2_spectrum.dat")
+om = a[:, 0]
+osz = a[:, 1]
+osz_x = a[:, 2]
+osz_y = a[:, 3]
+osz_z = a[:, 4]
+
+plot(om, osz, label="osz")
+plot(om, osz_x, label="osz_x")
+plot(om, osz_y, label="osz_y")
+plot(om, osz_z, label="osz_z")
+legend()
+show()
