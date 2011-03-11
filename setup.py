413c4b766109f4608a13cd83c34cc466ef9d836b
Ondrej Certik <ondrej@certik.cz>
2011-03-10 16:40:04 -0800

    Add qsnake.cmesh module

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
 setup(
     name = "libqsnake",
+    cmdclass = {'build_ext': build_ext},
     version = "0.1",
     packages = [
         'qsnake',
@@ -16,6 +19,7 @@ setup(
     package_data = {
         'qsnake.tests': ['phaml_data/domain.*'],
         },
+    ext_modules = [Extension("qsnake.cmesh", ["qsnake/cmesh.pyx"])],
     description = "Qsnake standard library",
     license = "BSD",
 )
