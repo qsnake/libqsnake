cb46ef374570d69f9fb82a4a4acaad68d66d5d8c
Ondrej Certik <ondrej@certik.cz>
2011-03-10 21:57:47 -0800

    Try to make it work

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from numpy.distutils.core import setup
+from numpy.distutils.extension import Extension
+from numpy.distutils.command.build_ext import build_ext as numpy_build_ext
+from Cython.Distutils import build_ext as cython_build_ext
 import numpy
+
+class build_ext(numpy_build_ext, cython_build_ext):
+    pass
 
 setup(
     name = "libqsnake",
-    cmdclass = {'build_ext': build_ext},
+#    cmdclass = {'build_ext': build_ext},
     version = "0.1",
     packages = [
         'qsnake',
@@ -21,10 +25,10 @@ setup(
         'qsnake.tests': ['phaml_data/domain.*'],
         },
     include_dirs=[numpy.get_include()],
-    ext_modules = [Extension("qsnake.cmesh", [
-        "qsnake/cmesh.pyx",
-        #"qsnake/fmesh.f90",
-        ])],
+    #    ext_modules = [Extension("qsnake.cmesh", [
+    #    "qsnake/cmesh.pyx",
+    #    #"qsnake/fmesh.f90",
+    #    ])],
     description = "Qsnake standard library",
     license = "BSD",
 )
