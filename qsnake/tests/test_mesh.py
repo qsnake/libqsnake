54bbcba5ca694e53cd7021a4bda1f684aec76c56
Ondrej Certik <ondrej@certik.cz>
2011-03-14 15:34:22 -0700

    Add more tests

--- qsnake/tests/test_mesh.py
+++ qsnake/tests/test_mesh.py
@@ -18,3 +18,20 @@ def test_mesh_log3():
     r = mesh_log(0, 10, 2, 3)
     correct = array([0, 2.2654091966098644, 5.4691816067802712, 10])
     assert (abs(r-correct) < eps).all()
+
+def test_mesh_log4():
+    r = mesh_log(0, 10, 2, 50)
+    a = (r[-1] - r[-2]) / (r[1] - r[0])
+    assert abs(a - 2) < eps
+
+    r = mesh_log(0, 10, 3, 50)
+    a = (r[-1] - r[-2]) / (r[1] - r[0])
+    assert abs(a - 3) < eps
+
+    r = mesh_log(0, 10, 4, 50)
+    a = (r[-1] - r[-2]) / (r[1] - r[0])
+    assert abs(a - 4) < eps
+
+    r = mesh_log(0, 10, 10.5, 50)
+    a = (r[-1] - r[-2]) / (r[1] - r[0])
+    assert abs(a - 10.5) < eps
