ba09d3081795005fe9ac5751775f7ad71d856716
Ondrej Certik <ondrej@certik.cz>
2011-03-14 16:13:23 -0700

    Rename fmesh to fmesh_wrapper

--- qsnake/cmesh.pyx
+++ qsnake/cmesh.pyx
@@ -1,14 +1,14 @@
 from numpy cimport ndarray
 from numpy import empty
 
-cimport fmesh
+from fmesh_wrapper cimport c_mesh_log, c_mesh_exp
 
 def mesh_log(double r_min, double r_max, double a, int N):
     cdef ndarray[double, mode="c"] mesh = empty(N+1, dtype="double")
-    fmesh.c_mesh_log(&r_min, &r_max, &a, &N, &mesh[0])
+    c_mesh_log(&r_min, &r_max, &a, &N, &mesh[0])
     return mesh
 
 def mesh_exp(double r_min, double r_max, double a, int N):
     cdef ndarray[double, mode="c"] mesh = empty(N+1, dtype="double")
-    fmesh.c_mesh_exp(&r_min, &r_max, &a, &N, &mesh[0])
+    c_mesh_exp(&r_min, &r_max, &a, &N, &mesh[0])
     return mesh
