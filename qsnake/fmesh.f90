605ee964cc3c86e3445a861d1cb1334ba51649da
Ondrej Certik <ondrej@certik.cz>
2011-03-14 15:13:31 -0700

    Implement mesh_exp()

--- qsnake/fmesh.f90
+++ qsnake/fmesh.f90
@@ -24,9 +24,28 @@ real(dp), intent(in) :: a
 integer, intent(in) :: N
 real(dp), intent(out) :: mesh(N+1)
 
+real(dp) :: a_new
+if (N > 1) then
+    a_new = a**(N/(N-1.0_dp))
+else if (N == 1) then
+    a_new = a
+else
+    stop "mesh_log() requires N >= 1"
+endif
+call mesh_exp(r_min, r_max, a, N, mesh)
+end subroutine
+
+subroutine mesh_exp(r_min, r_max, a, N, mesh)
+real(dp), intent(in) :: r_min
+real(dp), intent(in) :: r_max
+real(dp), intent(in) :: a
+integer, intent(in) :: N
+real(dp), intent(out) :: mesh(N+1)
+
 integer :: i
-do i = 1, N+1
-    mesh(i) = i
+if (N < 1) stop "mesh_exp() requires N >= 1"
+do i = 0, N
+    mesh(i+1) = i * (a - 1) / (a*N - i)  * (r_max - r_min) + r_min
 enddo
 end subroutine
 
