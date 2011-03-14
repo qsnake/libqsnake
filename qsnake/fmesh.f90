46583d9c8759ba9aa5501569a67a19bf6f652eda
Ondrej Certik <ondrej@certik.cz>
2011-03-14 15:38:43 -0700

    Implement mesh_hyp()

--- qsnake/fmesh.f90
+++ qsnake/fmesh.f90
@@ -45,6 +45,20 @@ real(dp), intent(out) :: mesh(N+1)
 integer :: i
 if (N < 1) stop "mesh_exp() requires N >= 1"
 do i = 0, N
+    mesh(i+1) = (exp(i*log(a)/N) - 1) / (a - 1)  * (r_max - r_min) + r_min
+enddo
+end subroutine
+
+subroutine mesh_hyp(r_min, r_max, a, N, mesh)
+real(dp), intent(in) :: r_min
+real(dp), intent(in) :: r_max
+real(dp), intent(in) :: a
+integer, intent(in) :: N
+real(dp), intent(out) :: mesh(N+1)
+
+integer :: i
+if (N < 1) stop "mesh_exp() requires N >= 1"
+do i = 0, N
     mesh(i+1) = i * (a - 1) / (a*N - i)  * (r_max - r_min) + r_min
 enddo
 end subroutine
