663688c67bb1134182d5b3299db734625fece783
Ondrej Certik <ondrej@certik.cz>
2011-03-15 20:16:30 -0700

    Rework mesh_nist2_direct()

--- examples/nist_meshes.py
+++ examples/nist_meshes.py
@@ -1,5 +1,5 @@
 from math import sqrt, log
-from numpy import exp, arange
+from numpy import exp, arange, array
 
 from qsnake.mesh import get_mesh_exp_params, mesh_exp
 
@@ -11,10 +11,17 @@ def mesh_nist1_direct(r_min, r_max, N):
     N = float(N)
     return r_min * a ** (n/N)
 
-def mesh_nist2_direct(a, b, N):
-    n = arange(N+1)
-    r = a * (exp(b*(n-1)) - 1)
-    return r[2:]
+def mesh_nist2_direct(a, b, r_max):
+    r = []
+    n = 1
+    while True:
+        r_n = a * (exp(b*(n-1)) - 1)
+        r.append(r_n)
+        if r_n > r_max:
+            break
+        n += 1
+    r = array(r)
+    return r[1:]
 
 def mesh_nist3_direct(r_min, r_max, N):
     # Uniform mesh in rho:
@@ -43,9 +50,9 @@ print r_min, r_max, a, N
 
 # 3
 # H
-#r = mesh_nist2_direct(4.34e-6/1, 0.002304, 7058)
+#r = mesh_nist2_direct(4.34e-6/1, 0.002304, 50)
 # U
-r = mesh_nist2_direct(4.34e-6/Z, 0.002304, 9021)
+r = mesh_nist2_direct(4.34e-6/Z, 0.002304, 50)
 r_min, r_max, a, N = get_mesh_exp_params(r)
 assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
 print "3)"
