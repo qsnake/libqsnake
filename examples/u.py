2da70c3d17df384545bb79cea5e6dfe36e523142
Ondrej Certik <ondrej@certik.cz>
2011-03-15 17:08:09 -0700

    Add Uranium example

---
+++ examples/u.py
@@ -0,0 +1,83 @@
+from qsnake.atom import solve_hydrogen_like_atom, ConvergeError
+from qsnake.mesh import n_minimize
+from sympy import TableForm
+from numpy import arange
+from json import dump, encoder
+
+def optimize(r_min=1e-6, r_max=50, N=700, solver="elk"):
+    def f(a):
+        r_min = params[0]
+        r_max = params[1]
+        N = params[2]
+        try:
+            r = solve_hydrogen_like_atom(92, {
+                    "r_min": r_min,
+                    "r_max": r_max,
+                    "a": a,
+                    "N": N,
+                }, {
+                    "solver": solver,
+                    "dirac": False
+                }, verbose=False)
+        except ConvergeError, e:
+            e.a = a
+            raise e
+        return r
+
+    a_min = 1
+    a_max = 1e8
+    # temporary:
+    #a_min = 3982631.666582 - 1
+    #a_max = 3982631.666582 + 1
+    params = [r_min, r_max, N]
+    done = False
+    while not done:
+        try:
+            print "Minimizing on (%f, %f)" % (a_min, a_max)
+            a_opt, error = n_minimize(f, ((a_min + a_max) / 2., a_min, a_max),
+                    method="brent")
+            done = True
+        except ConvergeError, e:
+            print "Didn't converge at a=", e.a
+            a_min = e.a
+    print
+    return a_opt, error, a_min, a_max
+
+def optimize_mesh(r_min=1e-8):
+    r_max = 50
+    N = 3000
+    solver = "dftatom"
+
+    a, error, a_min, a_max = optimize(r_min=r_min, r_max=r_max, N=N, solver=solver)
+    print "'a' is optimized:"
+    print TableForm([[r_min], [r_max], [a], [N]],
+                headings=(("r_min", "r_max", "a", "N"), ("Mesh parameters",)))
+    print "total error: %e" % error
+    result = {
+            "r_min": r_min,
+            "r_max": r_max,
+            "a": a,
+            "N": N,
+            "solver": solver,
+            "error": error,
+            "a_min": a_min,
+            "a_max": a_max,
+        }
+    return result
+
+a = 1e-10
+b = 1e-6
+results = []
+r_min_list = arange(b, a, -(b-a)/4.)
+r_min_list = [a, (a+b)/2, b]
+print "Optimizing for:"
+print r_min_list
+for r_min in r_min_list:
+    print "Calculating:", r_min
+    r = optimize_mesh(r_min)
+    results.append(r)
+    print "Done calculating:", r_min
+
+f = open("results.json", "w")
+encoder.FLOAT_REPR = lambda o: format(o, '.15e')
+dump(results, f)
