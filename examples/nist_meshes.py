58a32ce67e5c433841248105f74094b72a00ae8f
Ondrej Certik <ondrej@certik.cz>
2011-03-15 21:47:44 -0700

    Use TableForm

--- examples/nist_meshes.py
+++ examples/nist_meshes.py
@@ -3,6 +3,8 @@ from numpy import exp, arange, array
 
 from qsnake.mesh import get_mesh_exp_params, mesh_exp
 
+from sympy import TableForm
+
 def mesh_nist1_direct(r_min, r_max, N):
     r_min = float(r_min)
     r_max = float(r_max)
@@ -32,21 +34,19 @@ def mesh_nist3_direct(r_min, r_max, N):
 Z = 92
 eps = 1e-10
 
-print "NIST meshes used for Uranium"
+data = []
 
 # 1
 r = mesh_nist1_direct(1./(160*Z), 50, 15788)
 r_min, r_max, a, N = get_mesh_exp_params(r)
 assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
-print "1)"
-print r_min, r_max, "%e" % a, N
+data.append((r_min, r_max, "%e" % a, N))
 
 # 2
 r = mesh_nist1_direct(1e-6/Z, 800/sqrt(Z), 8000)
 r_min, r_max, a, N = get_mesh_exp_params(r)
 assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
-print "2)"
-print r_min, r_max, "%e" % a, N
+data.append((r_min, r_max, "%e" % a, N))
 
 # 3
 # H
@@ -55,12 +55,13 @@ print r_min, r_max, "%e" % a, N
 r = mesh_nist2_direct(4.34e-6/Z, 0.002304, 50)
 r_min, r_max, a, N = get_mesh_exp_params(r)
 assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
-print "3)"
-print r_min, r_max, "%e" % a, N
+data.append((r_min, r_max, "%e" % a, N))
 
 # 4
 r = mesh_nist3_direct(0.01e-4/Z, 50, 2837)
 r_min, r_max, a, N = get_mesh_exp_params(r)
 assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
-print "4)"
-print r_min, r_max, "%e" % a, N
+data.append((r_min, r_max, "%e" % a, N))
+
+print "NIST meshes used for Uranium:"
+print TableForm(data, headings=(("automatic", ("r_min", "r_max", "a", "N"))))
