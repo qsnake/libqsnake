89990ad2c5ca7502c3a37b459eb48ee3edca8e11
Ondrej Certik <ondrej@certik.cz>
2011-03-15 18:31:04 -0700

    Add the rest of the meshes

--- examples/nist_meshes.py
+++ examples/nist_meshes.py
@@ -1,7 +1,7 @@
 from math import sqrt
 
 from qsnake.mesh import (get_mesh_exp_params, mesh_exp, mesh_nist1_direct,
-        mesh_nist2_direct)
+        mesh_nist2_direct, mesh_nist3_direct)
 
 Z = 92
 eps = 1e-10
@@ -23,8 +23,18 @@ print "2)"
 print r_min, r_max, a, N
 
 # 3
+# H
+#r = mesh_nist2_direct(4.34e-6/1, 0.002304, 7058)
+# U
 r = mesh_nist2_direct(4.34e-6/Z, 0.002304, 9021)
 r_min, r_max, a, N = get_mesh_exp_params(r)
 assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
 print "3)"
+print r_min, r_max, a, N
+
+# 4
+r = mesh_nist3_direct(0.01e-4/Z, 50, 2837)
+r_min, r_max, a, N = get_mesh_exp_params(r)
+assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
+print "4)"
 print r_min, r_max, a, N
