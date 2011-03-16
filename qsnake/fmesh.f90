c927dd85495d01c2e62c2bb8bd5342af2543164c
Ondrej Certik <ondrej@certik.cz>
2011-03-15 17:37:47 -0700

    Implement a==1 case

--- qsnake/fmesh.f90
+++ qsnake/fmesh.f90
@@ -8,8 +8,18 @@ contains
 
 subroutine mesh_exp(r_min, r_max, a, N, mesh)
 ! Generates exponential mesh of N elements on [r_min, r_max]
-! The meaning of the parameter "a" is the fraction of the largest/smallest
-! elements in the mesh
+!
+! Input parameters:
+!     r_min, r_max .... The domain [r_min, r_max], the mesh will contain both
+!                       endpoints
+!     a ............... The fraction of the rightmost vs. leftmost elements of
+!                       the mesh (for a > 1 this means the "largest/smallest").
+!                       The only requirement is a > 0. For a == 1 a uniform
+!                       mesh will be returned.
+!     N ............... The number of elements in the mesh
+!
+! Output parameters:
+!     mesh(N+1) ....... The generated mesh
 real(dp), intent(in) :: r_min
 real(dp), intent(in) :: r_max
 real(dp), intent(in) :: a
@@ -18,17 +28,26 @@ real(dp), intent(out) :: mesh(N+1)
 
 integer :: i
 real(dp) :: alpha, beta
-if (N > 1) then
-    beta = log(a)/(N-1)
-    alpha = (r_max - r_min) / (exp(beta*N) - 1)
+if (a < 0) then
+    stop "mesh_exp: a > 0 required"
+else if (a == 1) then
+    alpha = (r_max - r_min) / N
     do i = 1, N+1
-        mesh(i) = alpha * (exp(beta*(i-1)) - 1) + r_min
+        mesh(i) = alpha * (i-1.0_dp) + r_min
     enddo
-else if (N == 1) then
-    mesh(1) = r_min
-    mesh(2) = r_max
 else
-    stop "mesh_exp() requires N >= 1"
+    if (N > 1) then
+        beta = log(a)/(N-1)
+        alpha = (r_max - r_min) / (exp(beta*N) - 1)
+        do i = 1, N+1
+            mesh(i) = alpha * (exp(beta*(i-1)) - 1) + r_min
+        enddo
+    else if (N == 1) then
+        mesh(1) = r_min
+        mesh(2) = r_max
+    else
+        stop "mesh_exp: N >= 1 required"
+    endif
 endif
 end subroutine
 
