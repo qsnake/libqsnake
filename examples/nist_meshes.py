16774320a7c41d5b98107ed0339fee78935bcefb
Ondrej Certik <ondrej@certik.cz>
2011-03-15 20:40:04 -0700

    Use exponential notation

--- examples/nist_meshes.py
+++ examples/nist_meshes.py
@@ -39,14 +39,14 @@ r = mesh_nist1_direct(1./(160*Z), 50, 15788)
 r_min, r_max, a, N = get_mesh_exp_params(r)
 assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
 print "1)"
-print r_min, r_max, a, N
+print r_min, r_max, "%e" % a, N
 
 # 2
 r = mesh_nist1_direct(1e-6/Z, 800/sqrt(Z), 8000)
 r_min, r_max, a, N = get_mesh_exp_params(r)
 assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
 print "2)"
-print r_min, r_max, a, N
+print r_min, r_max, "%e" % a, N
 
 # 3
 # H
@@ -56,11 +56,11 @@ r = mesh_nist2_direct(4.34e-6/Z, 0.002304, 50)
 r_min, r_max, a, N = get_mesh_exp_params(r)
 assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
 print "3)"
-print r_min, r_max, a, N
+print r_min, r_max, "%e" % a, N
 
 # 4
 r = mesh_nist3_direct(0.01e-4/Z, 50, 2837)
 r_min, r_max, a, N = get_mesh_exp_params(r)
 assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
 print "4)"
-print r_min, r_max, a, N
+print r_min, r_max, "%e" % a, N
