1733ec108d02eed58e175f0a2060a7b20d5b892b
Ondrej Certik <ondrej@certik.cz>
2011-03-15 18:09:56 -0700

    Add 3) to nist meshes

---
+++ examples/nist_meshes.py
@@ -0,0 +1,30 @@
+from math import sqrt
+
+from qsnake.mesh import (get_mesh_exp_params, mesh_exp, mesh_nist1_direct,
+        mesh_nist2_direct)
+
+Z = 92
+eps = 1e-10
+
+print "NIST meshes used for Uranium"
+
+# 1
+r = mesh_nist1_direct(1./(160*Z), 50, 15788)
+r_min, r_max, a, N = get_mesh_exp_params(r)
+assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
+print "1)"
+print r_min, r_max, a, N
+
+# 2
+r = mesh_nist1_direct(1e-6/Z, 800/sqrt(Z), 8000)
+r_min, r_max, a, N = get_mesh_exp_params(r)
+assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
+print "2)"
+print r_min, r_max, a, N
+
+# 3
+r = mesh_nist2_direct(4.34e-6/Z, 0.002304, 9021)
+r_min, r_max, a, N = get_mesh_exp_params(r)
+assert (abs(mesh_exp(r_min, r_max, a, N) - r) < eps).all()
+print "3)"
+print r_min, r_max, a, N
